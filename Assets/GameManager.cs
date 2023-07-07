5d764bba9f53c851dc0b74cb09bb594c0ea6e360
Gargantus <43757136+Gargantus@users.noreply.github.com>
2023-07-07 21:28:05 +0200

    Imported the package

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] Tiles;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
